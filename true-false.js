95e2799607e802c9b8545c76257815c59a4c1ae8
joyonto <coderjoyonto@gmail.com>
2021-04-30 02:02:46 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// falsy:
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+// Truthy:
+//'0', ' ', {}, [], function
+
+
+let name;
+console.log(name)
+if(name){
+    console.log("this condition is true");
+}
+else{
+    console.log("this condition is false")
+}
