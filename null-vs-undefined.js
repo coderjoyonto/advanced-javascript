57af7d8302c0e09c00d8055fd8729e6ed74a905d
joyonto <coderjoyonto@gmail.com>
2021-04-30 02:32:47 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+let hello;
+
+function add(num1, num2){
+    const total = (num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2); 
+}
+
+const premik = {id:12121, phone:999};
+console.log(premik.gf)
+
+let fun = undefined;
+console.log(fun);
+
